25d04847b6e2dbcb815eccb637f39cd00e8a8cfc
Hchnr <hechenrui123@gmail.com>
2025-08-08 16:22:05 +0800

    update: compile lm.forward_text

--- flmaudio/models/streaming_flmaudio.py
+++ flmaudio/models/streaming_flmaudio.py
@@ -85,6 +85,8 @@ class LMGen(StreamingModule[_LMGenState]):
         )
         past_key_values = DynamicCache()
 
+        lm_model.forward = torch.compile(lm_model.forward)
+
         return _LMGenState(cache, initial, lm_model.forward, self.depformer_step, past_key_values, offset=0, audio_speak_tokens_cache=[], text_tokens_cache=[])
 
     @torch.no_grad()
@@ -130,7 +132,7 @@ class LMGen(StreamingModule[_LMGenState]):
 
         t_s = time.time()
         lm_outputs = state.forward_text(**model_input)
-        # log("info", f"lm.forward_text: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
+        log("info", f"lm.forward_text: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
         state.past_key_values = lm_outputs.past_key_values
         last_text_token_logits = lm_outputs.logits[:, -1:, :]
         last_hidden_states = lm_outputs.hidden_states[:, -1, :]
@@ -147,7 +149,7 @@ class LMGen(StreamingModule[_LMGenState]):
             repetition_penalty=self.repetition_penalty_text if rep_window_input_ids is not None else 1.0,
             input_ids=rep_window_input_ids,
         )
-        # log("info", f"sample_text: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
+        log("info", f"sample_text: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
 
         state.text_tokens_cache.append(sampled_text_token)
         if len(state.text_tokens_cache) > self.repetition_penalty_window_text:
@@ -156,7 +158,7 @@ class LMGen(StreamingModule[_LMGenState]):
         sampled_text_token = sampled_text_token[0]  # shape is [B]
 
         audio_tokens = state.depformer_step(last_hidden_states)
-        # log("info", f"depformer_step: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
+        log("info", f"depformer_step: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
 
         state.offset += 1
         position = state.offset % CT
@@ -194,7 +196,8 @@ class LMGen(StreamingModule[_LMGenState]):
                 transformer_output_states=last_hidden_states,
                 audio_input_ids=decoded_audio_tokens
             ) # [B, K, vocab_size+1]: logis
-            log("info", f"forward_audio[{i}]: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
+            if i == 0:
+                log("info", f"forward_audio[{i}]: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
             current_aud_logit = audio_logits[:, -1:, :]
 
             sampled_audio_token = sample_token(
@@ -207,7 +210,8 @@ class LMGen(StreamingModule[_LMGenState]):
                 input_ids=rep_window_input_ids[:, i] if rep_window_input_ids is not None else None
                 ).unsqueeze(1)   # [B, 1, 1(k)]   ->[B, 1(k)]
             decoded_audio_tokens = torch.concat([decoded_audio_tokens, sampled_audio_token], dim=1)   # [B, 0] [B, 1(k)]
-            log("info", f"sample_audio[{i}]: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
+            if i == 0:
+                log("info", f"sample_audio[{i}]: {1000 * (time.time() - t_s):.1f}ms"); t_s = time.time()
 
         state.audio_speak_tokens_cache.append(decoded_audio_tokens)
 
